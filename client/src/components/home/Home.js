cf77f578500ada9b8a09460ece90bfcd464311f3
Fayçal <fay68390@gmail.com>
2019-02-21 16:48:09 +0100

    first commit

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Home.css';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="home">
+                <div>
+                    <h1>Hi, I'm Fayçal.</h1>
+                    <h2 className="subtitle">I'm a learning full-stack developper</h2>
+                </div>
+            </div>
+        )
+    }
+}
