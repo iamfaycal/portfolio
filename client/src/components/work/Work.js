6ef05ba650922844bb7b3960e25eff8f00ae42e1
Fayçal <fay68390@gmail.com>
2019-06-07 14:12:17 +0200

    merge

--- client/src/components/work/Work.js
+++ client/src/components/work/Work.js
@@ -5,6 +5,16 @@ export default class Work extends Component {
         return (
         <div>
             <h2>Réalisations</h2>
+            <div className="realisations-container">
+                <div className="realisation">
+                    <div className="realisation-inner">
+                        <h3>Ma réalisation</h3>
+                        <hr/>
+                        <h4>Une lègère description &rarr;</h4>
+                        <img src="https://picsum.photos/640/360" alt=""/>
+                    </div>
+                </div>
+            </div>
         </div>
         )
     }
