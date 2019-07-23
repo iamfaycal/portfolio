5535f1ec6c6dc8b50664104b9235607af9be1f58
Fayçal <fay68390@gmail.com>
2019-07-23 15:10:20 +0200

    deployement stuff

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
@@ -16,6 +17,14 @@ app.use(bodyParser.json());
 // app.use("/api/login", login);
 // app.use("/api/realisations", realisations);
 app.use("/api/message", message);
+
+// server static if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', () => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
