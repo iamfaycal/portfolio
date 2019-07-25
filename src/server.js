c4770624765ae70105d4eeb525d3da871f47b98c
Fayçal <fay68390@gmail.com>
2019-07-25 13:12:10 +0200

    server default production route

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use("/api/message", message);
 
 // server static if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    //app.use(express.static('client/build'));
     app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build/index.html'));
     })
