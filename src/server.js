534336ac23f69ae0792342908c9c00f2949f493b
Fayçal <fay68390@gmail.com>
2019-07-25 12:05:04 +0200

    server default production route

--- src/server.js
+++ src/server.js
@@ -20,10 +20,10 @@ app.use("/api/message", message);
 
 // server static if in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    /*app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })*/
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    })
 }
 
 // console.log that your server is up and running
